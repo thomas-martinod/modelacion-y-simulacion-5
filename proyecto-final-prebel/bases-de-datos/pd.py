68a9559c631692b7ba2c4ceda17fe0e77f8bdecd
Thomas Martinod <110356084+thomas-martinod@users.noreply.github.com>
2024-05-21 11:36:44 -0500

    hi

--- proyecto-final-prebel/bases-de-datos/pd.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel(r'C:\Users\thomm\Documents\GitHub\modelacion-y-simulacion-5\proyecto-final-prebel\bases-de-datos\datos-discretos.xlsx', sheet_name='vReportsCalculationsSpanish')
-
-
-
